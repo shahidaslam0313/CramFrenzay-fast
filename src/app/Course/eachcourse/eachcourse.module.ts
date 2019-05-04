924b8b2f2812c4207215c5f93af565d7e96bc5d5
aqsach <35590350+aqsach@users.noreply.github.com>
2019-05-04 15:14:48 +0500

    eachcourese

--- src/app/Course/eachcourse/eachcourse.module.ts
+++ src/app/Course/eachcourse/eachcourse.module.ts
@@ -5,6 +5,13 @@ import {RouterModule, Routes} from "@angular/router";
 import { MaterialModule } from '../../app.module';
 import { CommonModule} from "@angular/common";
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {VgBufferingModule} from "videogular2/buffering";
+import {VgCoreModule} from "videogular2/core";
+import {VgOverlayPlayModule} from "videogular2/overlay-play";
+import {VgControlsModule} from "videogular2/controls";
+import {CKEditorModule} from "ng2-ckeditor";
+import {AddVideoComponent, IntroVideoComponent} from "../../userdashboard/coursevideo/coursevideo.component";
+import {VideoShowDialogComponent} from "../../userdashboard/coursevideo/video-show-dialog/video-show-dialog.component";
 // import { AddtocartComponent } from './../../addtocart/addtocart.component';
 const EachcourseRoutes: Routes = [
   { path: '', component: EachcourseComponent }
@@ -15,12 +22,24 @@ const EachcourseRoutes: Routes = [
     RouterModule.forChild(EachcourseRoutes),
     CommonModule,
     MaterialModule,
-    FormsModule, ReactiveFormsModule
+    FormsModule, ReactiveFormsModule,
+    VgBufferingModule,
+    VgCoreModule,
+    VgOverlayPlayModule,
+    VgControlsModule,
+    CKEditorModule
   ],
   declarations: [
     EachcourseComponent,
+    VideoShowDialogComponent
     // AddtocartComponent
   ],
+  entryComponents: [
+
+    VideoShowDialogComponent
+
+  ]
+
   // providers: [AddtocartService]
 })
 export class EachcourseModule {}
