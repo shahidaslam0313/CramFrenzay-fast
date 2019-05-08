587d6977f3e92b3b93db19cf78b52e702a787bd1
aqsach <35590350+aqsach@users.noreply.github.com>
2019-05-08 17:38:32 +0500

    mycart

--- src/app/userdashboard/mycart/mycart.component.ts
+++ src/app/userdashboard/mycart/mycart.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { headerservice } from '../../includes/header/header.service';
+import { Config } from '../../Config';
+import { ActivatedRoute, Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-mycart',
@@ -6,11 +10,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mycart.component.scss']
 })
 export class MycartComponent implements OnInit {
-
-  constructor() { }
+  itemscount;
+  totalprice;
+  FlashCard;
+  Courses;
+  Notes;
+  Book;
+  record;
+  public Imageurl = Config.Imageurlget;
+  constructor(public header: headerservice,) { }
 
   ngOnInit() {
     window.scroll(0,0);
+    this.showCartItems();
   }
-
+  showCartItems() {
+    this.header.showCartItem().subscribe(Data => {
+      this.itemscount = Data.counts;
+      this.totalprice = Data.sum
+this.record = Data.Cart;
+console.log(this.record)
+      for (let vall in Data.Cart) {
+        if (Data.Cart[vall].course) {
+          this.Courses(Data.Cart[vall]);
+          console.log(this.Courses);
+        }
+        else if (Data.Cart[vall].notes) {
+          this.Notes(Data.Cart[vall]);
+          console.log(this.Notes);
+        }
+        // else if (Data.Cart[vall].book) {
+        //   this.Book(Data.Cart[vall]);
+        // }
+        // else if (Data.Cart[vall].flashcard) {
+        //   this.FlashCard(Data.Cart[vall]);
+        // }
+      }
+    })
+  }
 }
