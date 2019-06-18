2f31db89bd9104b588266092949a05f9a2e60652
ShameemAnwar <48752790+ShameemAnwar@users.noreply.github.com>
2019-06-18 11:53:18 +0500

    see more of main page

--- src/app/MainPage/seemoremp/seemoremp.component.ts
+++ src/app/MainPage/seemoremp/seemoremp.component.ts
@@ -7,7 +7,7 @@ import { ActivatedRoute } from "@angular/router";
 import swal from 'sweetalert2';
 import { PagerService } from '../../paginator.service';
 import { isPlatformBrowser } from '@angular/common';
-import {GlobalService} from '../../global.service';
+import { GlobalService } from '../../global.service';
 import { AcceptofferComponent } from 'app/acceptoffer/acceptoffer.component';
 import { MatDialog } from '@angular/material';
 import { FormControl, Validators, FormGroup, NgForm } from '@angular/forms';
@@ -15,7 +15,7 @@ import { headerservice } from 'app/includes/header/header.service';
 import { DataService } from 'app/data.service';
 import { mainpageservice } from '../mainpage/mainpage.service';
 import { WishlistService } from 'app/wishlist/wishlist.service';
-import {BidHistoryService} from "../../bid-history/bid-history.service";
+import { BidHistoryService } from "../../bid-history/bid-history.service";
 
 
 @Component({
@@ -44,7 +44,7 @@ export class SeemorempComponent implements OnInit {
     ])
   })
   private sub: Subscription;
-  constructor( private bidings: BidHistoryService,private headServ: headerservice, private Data: DataService,private see: WishlistService,private mainpage: mainpageservice, private pagerService: PagerService, private seemore: SeemorempService, private router: Router, private route: ActivatedRoute,  @Inject(PLATFORM_ID) private platformId: Object , private global: GlobalService, public dialogRef: MatDialog) {
+  constructor(private bidings: BidHistoryService, private headServ: headerservice, private Data: DataService, private see: WishlistService, private mainpage: mainpageservice, private pagerService: PagerService, private seemore: SeemorempService, private router: Router, private route: ActivatedRoute, @Inject(PLATFORM_ID) private platformId: Object, private global: GlobalService, public dialogRef: MatDialog) {
     this.route.params.subscribe(params => { });
     this.sub = this.route.params.subscribe(params => {
       this.name = +params['name'];
@@ -66,12 +66,12 @@ export class SeemorempComponent implements OnInit {
   }
   name;
   ngOnInit() {
-      window.scroll(0,0)
+    window.scroll(0, 0)
 
   }
   courses(id) {
     if (this.check_login() == true) {
-      this.router.navigate(['/payment'], {queryParams: {courseid : id}});
+      this.router.navigate(['/payment'], { queryParams: { courseid: id } });
     } else if (this.check_login() == false) {
       this.sweetalertsignin();
       this.router.navigate(['/login']);
@@ -79,7 +79,7 @@ export class SeemorempComponent implements OnInit {
   }
   flashcard(id) {
     if (this.check_login() == true) {
-      this.router.navigate(['/payment'], {queryParams: {cardsid : id}});
+      this.router.navigate(['/payment'], { queryParams: { cardsid: id } });
       localStorage.setItem('flashcard', id);
     } else if (this.check_login() == false) {
       this.sweetalertsignin();
@@ -88,7 +88,7 @@ export class SeemorempComponent implements OnInit {
   }
   Books(id) {
     if (this.check_login() == true) {
-      this.router.navigate(['/payment'], {queryParams: {bookid : id}});
+      this.router.navigate(['/payment'], { queryParams: { bookid: id } });
     } else if (this.check_login() == false) {
       this.sweetalertsignin();
       this.router.navigate(['/login']);
@@ -132,11 +132,11 @@ export class SeemorempComponent implements OnInit {
       this.sweetalertsignin();
       this.router.navigate(['/login']);
     }
-    this.showhistory(notes, this.course, this.flashcard , this.book)
+    this.showhistory(notes, this.course, this.flashcard, this.book)
   }
-  showhistory(notes,course, flashcard, book ) {
-    return this.global.offerhistory(notes, course, flashcard ,book )
- }
+  showhistory(notes, course, flashcard, book) {
+    return this.global.offerhistory(notes, course, flashcard, book)
+  }
   openDialog2(course): void {
     if (this.check_login() == true) {
       const dialogRef = this.dialogRef.open(AcceptofferComponent, {
@@ -153,7 +153,7 @@ export class SeemorempComponent implements OnInit {
       this.sweetalertsignin();
       this.router.navigate(['/login']);
     }
-    this.showhistory(this.notes, course, this.flashcard , this.book)
+    this.showhistory(this.notes, course, this.flashcard, this.book)
   }
   openDialog3(book): void {
     if (this.check_login() == true) {
@@ -171,7 +171,7 @@ export class SeemorempComponent implements OnInit {
       this.sweetalertsignin();
       this.router.navigate(['/login']);
     }
-    this.showhistory(this.notes, this.course, this.flashcard , book)
+    this.showhistory(this.notes, this.course, this.flashcard, book)
   }
   openDialog4(flashcard): void {
     if (this.check_login() == true) {
@@ -189,11 +189,11 @@ export class SeemorempComponent implements OnInit {
       this.sweetalertsignin();
       this.router.navigate(['/login']);
     }
-    this.showhistory(this.notes, this.course,flashcard , this.book)
+    this.showhistory(this.notes, this.course, flashcard, this.book)
   }
   checkmainpage(id) {
     if (this.check_login() == true) {
-      this.router.navigate(['/payment'], {queryParams: {notesid : id}});
+      this.router.navigate(['/payment'], { queryParams: { notesid: id } });
     }
     else if (this.check_login() == false) {
       this.sweetalertsignin();
@@ -249,13 +249,25 @@ export class SeemorempComponent implements OnInit {
         title: 'Item successfully added to watch list',
         showConfirmButton: false,
         timer: 2000
-      })
+      });
+      if (notes) {
+        this.setPagenotes(1);
+      }
+      else if (course) {
+        this.setPagecourse(1);
+      }
+      else if (flashcard) {
+        this.setPagflash(1);
+      }
+      else if (book) {
+        this.setPagebooks(1);
+      }
       this.headServ.showwishlist().subscribe(wishList => {
         this.wishlist = wishList;
         this.Data.emittedData(this.wishlist);
       })
     }, error => {
-      if (error.status == 404){
+      if (error.status == 404) {
         swal({
           type: 'warning',
           title: 'This item is already exist in your watch list',
@@ -263,7 +275,7 @@ export class SeemorempComponent implements OnInit {
           timer: 2000
         })
       }
-      else if(error.status == 406){
+      else if (error.status == 406) {
         swal({
           type: 'error',
           title: 'Item Already Purchased',
@@ -271,7 +283,7 @@ export class SeemorempComponent implements OnInit {
           timer: 2000
         })
       }
-      
+
     });
 
   }
@@ -296,10 +308,10 @@ export class SeemorempComponent implements OnInit {
     });
   }
 
-  bidingonbook(f:NgForm) {
-    if(this.bidform.controls.bidamount.valid){
-    this.global.bidonbook( this.bidbookid, this.bidform.value['bidamount'])
-      .subscribe(Res => {
+  bidingonbook(f: NgForm) {
+    if (this.bidform.controls.bidamount.valid) {
+      this.global.bidonbook(this.bidbookid, this.bidform.value['bidamount'])
+        .subscribe(Res => {
           swal({
             type: 'success',
             title: 'Your bid is listed',
@@ -307,26 +319,26 @@ export class SeemorempComponent implements OnInit {
             timer: 5500
           });
         },
-        error => {
-          if(error.status === 403)
-            swal({
-              type: 'error',
-              title: 'Bid higher amount',
-              showConfirmButton: false,
-              timer: 5500
-            });
-        },);
-        
-      }
-      else 
+          error => {
+            if (error.status === 403)
+              swal({
+                type: 'error',
+                title: 'Bid higher amount',
+                showConfirmButton: false,
+                timer: 5500
+              });
+          });
+
+    }
+    else
       swal({
         type: 'error',
         title: 'Bid amount is required',
         showConfirmButton: false,
         timer: 1500
       });
-      f.resetForm()
-    }
+    f.resetForm()
+  }
   bidonnotes;
   bidnotesid(id) {
     if (this.check_login() == true) {
@@ -339,36 +351,36 @@ export class SeemorempComponent implements OnInit {
     }
   }
   biding(f: NgForm) {
-  if(this.bidform.controls.bidamount.valid){
-    this.global.bidnotes( this.bidonnotes, this.bidform.value['bidamount'])
-    .subscribe(Res => {
+    if (this.bidform.controls.bidamount.valid) {
+      this.global.bidnotes(this.bidonnotes, this.bidform.value['bidamount'])
+        .subscribe(Res => {
+          swal({
+            type: 'success',
+            title: 'Your bid is listed',
+            showConfirmButton: false,
+            timer: 5500
+          });
+        },
+          error => {
+            if (error.status == 403)
+              swal({
+                type: 'error',
+                title: 'Bid higher amount',
+                showConfirmButton: false,
+                timer: 2000
+              });
+          },
+        );
+      f.resetForm()
+    }
+    else
       swal({
-        type: 'success',
-        title: 'Your bid is listed',
+        type: 'error',
+        title: 'Bid amount is required',
         showConfirmButton: false,
-        timer: 5500
+        timer: 1500
       });
-    },
-    error => {
-      if (error.status == 403)
-        swal({
-          type: 'error',
-          title: 'Bid higher amount',
-          showConfirmButton: false,
-          timer: 2000
-        });
-    },
-  );
-  f.resetForm()
-}
-else 
-swal({
-  type: 'error',
-  title: 'Bid amount is required',
-  showConfirmButton: false,
-  timer: 1500
-});
-}
+  }
   bidingcourse;
   ////////////////biding on courses///////////
   bidcourseid(id) {
@@ -383,9 +395,9 @@ swal({
 
   }
   bidc(f: NgForm) {
-    if(this.bidform.controls.bidamount.valid){
-    this.global.bidoncourses( this.bidingcourse, this.bidform.value['bidamount'])
-      .subscribe(Res => {
+    if (this.bidform.controls.bidamount.valid) {
+      this.global.bidoncourses(this.bidingcourse, this.bidform.value['bidamount'])
+        .subscribe(Res => {
           swal({
             type: 'success',
             title: 'Your bid is listed',
@@ -393,30 +405,30 @@ swal({
             timer: 5500
           });
         },
-        error => {
-          if(error.status === 403)
-            swal({
-              type: 'error',
-              title: 'Bid higher amount',
-              showConfirmButton: false,
-              timer: 5500
-            });
-        },);
-        
-      }
-      else 
+          error => {
+            if (error.status === 403)
+              swal({
+                type: 'error',
+                title: 'Bid higher amount',
+                showConfirmButton: false,
+                timer: 5500
+              });
+          });
+
+    }
+    else
       swal({
         type: 'error',
         title: 'Bid amount is required',
         showConfirmButton: false,
         timer: 1500
       });
-      f.resetForm()
-    }
+    f.resetForm()
+  }
   cardid;
-  getcardid(id){
+  getcardid(id) {
 
-    if (this.check_login() == true){
+    if (this.check_login() == true) {
       this.cardid = id;
       this.getcardbidhistory(this.cardbid);
     }
@@ -425,10 +437,10 @@ swal({
       this.router.navigate(['/login']);
     }
   }
-  bidcard(f:NgForm) {
-    if(this.bidform.controls.bidamount.valid){
-    this.global.bidoncards( this.cardid, this.bidform.value['bidamount'])
-      .subscribe(Res => {
+  bidcard(f: NgForm) {
+    if (this.bidform.controls.bidamount.valid) {
+      this.global.bidoncards(this.cardid, this.bidform.value['bidamount'])
+        .subscribe(Res => {
           swal({
             type: 'success',
             title: 'Your bid is listed',
@@ -436,27 +448,26 @@ swal({
             timer: 5500
           });
         },
-        error => {
-          if(error.status === 403)
-            swal({
-              type: 'error',
-              title: 'Bid higher amount',
-              showConfirmButton: false,
-              timer: 5500
-            });
-        },);
-        
-      }
-      else 
+          error => {
+            if (error.status === 403)
+              swal({
+                type: 'error',
+                title: 'Bid higher amount',
+                showConfirmButton: false,
+                timer: 5500
+              });
+          });
+    }
+    else
       swal({
         type: 'error',
         title: 'Bid amount is required',
         showConfirmButton: false,
         timer: 1500
       });
-      f.resetForm()
-    }
-  addcart( notes, course, book, flashcard){
+    f.resetForm()
+  }
+  addcart(notes, course, book, flashcard) {
     if (this.check_login() == true) {
       this.global.addtocart(notes, course, book, flashcard).subscribe(data => {
         swal({
@@ -465,6 +476,18 @@ swal({
           showConfirmButton: false,
           timer: 4500
         });
+        if (notes) {
+          this.setPagenotes(1);
+        }
+        else if (course) {
+          this.setPagecourse(1);
+        }
+        else if (flashcard) {
+          this.setPagflash(1);
+        }
+        else if (book) {
+          this.setPagebooks(1);
+        }
         this.headServ.showCartItem().subscribe(cartitem => {
           this.cartitem = cartitem;
           this.Data.emittData(this.cartitem);
@@ -477,84 +500,123 @@ swal({
             showConfirmButton: false,
             timer: 4500
           })
-          else if ( error.status === 406)
+        else if (error.status === 406)
           swal({
             type: 'error',
             title: 'Item Already Purchased',
             showConfirmButton: false,
             timer: 4500
           })
-        });
+      });
+    }
+    else if (this.check_login() == false) {
+      this.sweetalertsignin();
+      this.router.navigate(['/login']);
+    }
   }
-  else if (this.check_login() == false) {
-    this.sweetalertsignin();
-    this.router.navigate(['/login']);
+  delflashfromcart(event) {
+    this.global.delcart(event.cart).subscribe(data => {
+      swal({
+        type: 'success',
+        title: 'Item successfully deleted from cart',
+        showConfirmButton: false,
+        timer: 1500
+      });
+      this.setPagflash(1);
+    });
   }
-}
-delfromcart(event) {
-  this.global.delcart(event.cart).subscribe(data => {
-    swal({
-      type: 'success',
-      title: 'Item successfully deleted from cart',
-      showConfirmButton: false,
-      timer: 1500
+  delBookfromcart(event) {
+    this.global.delcart(event.cart).subscribe(data => {
+      swal({
+        type: 'success',
+        title: 'Item successfully deleted from cart',
+        showConfirmButton: false,
+        timer: 1500
+      });
+      this.setPagebooks(1);
     });
-  });
-}
-getcartitems(){
-  this.mainpage.showCartItem().subscribe(data =>{
-  })
-}
-delCourseFwishList(event) {
-  this.see.delwishlist(event.wishlist).subscribe(data => {
-    swal({
-      type: 'success',
-      title: 'Item successfully deleted from watch list',
-      showConfirmButton: false,
-      timer: 1500
+  }
+  delCoursefromcart(event) {
+    this.global.delcart(event.cart).subscribe(data => {
+      swal({
+        type: 'success',
+        title: 'Item successfully deleted from cart',
+        showConfirmButton: false,
+        timer: 1500
+      });
+      this.setPagecourse(1);
     });
-  });
-}
-delFlaskFwishList(event) {
-  this.see.delwishlist(event.wishlist).subscribe(data => {
-    swal({
-      type: 'success',
-      title: 'Item successfully deleted from watch list',
-      showConfirmButton: false,
-      timer: 1500
+  }
+  delNotesfromcart(event) {
+    this.global.delcart(event.cart).subscribe(data => {
+      swal({
+        type: 'success',
+        title: 'Item successfully deleted from cart',
+        showConfirmButton: false,
+        timer: 1500
+      });
+      this.setPagenotes(1);
     });
-  });
-}
-delBookFwishList(event) {
-  this.see.delwishlist(event.wishlist).subscribe(data => {
-    swal({
-      type: 'success',
-      title: 'Item successfully deleted from watch list',
-      showConfirmButton: false,
-      timer: 1500
-    });
-  });
-}
-delNotesFtrendingNow(event){
-  this.mainpage.delwishlist(event.wishlist).subscribe(data => {
-    swal({
-      type: 'success',
-      title: 'Item successfully deleted from watch list',
-      showConfirmButton: false,
-      timer: 1500
+  }
+  getcartitems() {
+    this.mainpage.showCartItem().subscribe(data => {
     })
-
-  });
-}
+  }
+  delCourseFwishList(event) {
+    this.see.delwishlist(event.wishlist).subscribe(data => {
+      swal({
+        type: 'success',
+        title: 'Item successfully deleted from watch list',
+        showConfirmButton: false,
+        timer: 1500
+      });
+      this.setPagecourse(1);
+    });
+  }
+  delFlaskFwishList(event) {
+    this.see.delwishlist(event.wishlist).subscribe(data => {
+      swal({
+        type: 'success',
+        title: 'Item successfully deleted from watch list',
+        showConfirmButton: false,
+        timer: 1500
+      });
+      this.setPagflash(1);
+    });
+  }
+  delBookFwishList(event) {
+    this.see.delwishlist(event.wishlist).subscribe(data => {
+      swal({
+        type: 'success',
+        title: 'Item successfully deleted from watch list',
+        showConfirmButton: false,
+        timer: 1500
+      });
+      this.setPagebooks(1);
+    });
+  }
+  delNotesFtrendingNow(event) {
+    this.mainpage.delwishlist(event.wishlist).subscribe(data => {
+      swal({
+        type: 'success',
+        title: 'Item successfully deleted from watch list',
+        showConfirmButton: false,
+        timer: 1500
+      });
+      this.setPagenotes(1);
+    });
+  }
 
   notebid;
   coursebid;
   notes;
-////////////note biding history ///////////////
+  ////////////note biding history ///////////////
   getnotebidhistory(id) {
     this.bidings.notebidhistory(this.bidonnotes).subscribe(data => {
       this.notebid = data;
-      this.notes = data['Highest Bid'];})}
+      this.notes = data['Highest Bid'];
+    })
+  }
   ////////////course  biding history////////
   getcoursebidhistory(id) {
     this.bidings.coursebidhistory(this.bidingcourse).subscribe(data => {
